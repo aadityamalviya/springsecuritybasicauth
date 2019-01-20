ab33a03f67af9852efc7ce02e527abcae71778ad
aadityamalviya <adityamalviya@icloud.com>
2019-01-20 11:26:10 +0530

    initial commit project structure

---
+++ src/main/java/com/springsecuritybasicauth/demo/SpringsecuritybasicauthApplication.java
@@ -0,0 +1,14 @@
+package com.springsecuritybasicauth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecuritybasicauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsecuritybasicauthApplication.class, args);
+	}
+
+}
+
